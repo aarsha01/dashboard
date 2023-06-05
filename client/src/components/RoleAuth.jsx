b7b5a5c0f34b42ed4495ef2f541c0b4e705f063d
dheeraj <dheerajnunni@gmail.com>
2023-06-05 17:35:27 +0530

    bug fixes

--- client/src/components/RoleAuth.jsx
+++ client/src/components/RoleAuth.jsx
@@ -5,13 +5,13 @@ import configVariables from "../Constants/configVariables";
 
 const RoleAuth = ({ allowedRoles }) => {
   const location = useLocation();
-  console.log(localStorage.get(configVariables.USER_ROLE),allowedRoles);
-  return allowedRoles.includes(localStorage.get(configVariables.USER_ROLE))? (
+  console.log(localStorage.getItem(configVariables.USER_ROLE),allowedRoles);
+  return allowedRoles.includes(localStorage.getItem(configVariables.USER_ROLE))? (
     <Outlet />
-  ) : localStorage.get(configVariables.USER_ID) ?(
+  ) : localStorage.getItem(configVariables.USER_ID) ?(
     <Navigate to="/unauthorized" state={{ from: location }} replace />
   ) : (
-    <Navigate to="/signup" state={{ from: location }} replace />
+    <Navigate to="/login_page" state={{ from: location }} replace />
   );
 };
 

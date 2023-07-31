eefd61f2879ab46f26f3220e05434e11111728cf
Your Name <jasirhussain2002@gmail.com>
2023-07-31 16:14:20 +0530

    changes

--- client/src/helper/callApi.js
+++ client/src/helper/callApi.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default function callApi(endpoint, data={}) {
     return new Promise((resolve, reject) => {
-        const env = 'DEV'
-        const BASE_URL = env === 'PRODUCTION' ? 'https://gps-cidre-352715.el.r.appspot.com' : 'http://localhost:3000'
+        const env = 'PRODUCTION'
+        const BASE_URL = env === 'PRODUCTION' ? 'http://54.212.174.138:3000' : 'http://localhost:3000'
         const url = `${BASE_URL}/api/${endpoint}`  
         // const config = { headers: { "Authorization": `Bearer ${localStorage.getItem('user_token') && JSON.parse(localStorage.getItem('user_token')).value}` } }
         const body = data
